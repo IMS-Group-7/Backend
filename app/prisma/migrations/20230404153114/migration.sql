e9bce7a90b7c9b0f5b3cfd20f0945452ff2e2ff6
h-khalaf <h.khalaf723@gmail.com>
2023-04-04 17:34:15 +0200

    Prisma database migration
    Co-Authored-By: boiledsauce <85931509+boiledsauce@users.noreply.github.com>

---
+++ app/prisma/migrations/20230404153114/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE `Coordinates` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `mowerId` INTEGER NOT NULL,
+    `x` INTEGER NOT NULL,
+    `y` INTEGER NOT NULL,
+    `is_boundary` BOOLEAN NOT NULL,
+    `timestamp` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CollisionAvoidances` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `mowerId` INTEGER NOT NULL,
+    `x` INTEGER NOT NULL,
+    `y` INTEGER NOT NULL,
+    `imagePath` VARCHAR(191) NOT NULL,
+    `object` VARCHAR(191) NOT NULL,
+    `timestamp` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Coordinates` ADD CONSTRAINT `Coordinates_mowerId_fkey` FOREIGN KEY (`mowerId`) REFERENCES `Mower`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CollisionAvoidances` ADD CONSTRAINT `CollisionAvoidances_mowerId_fkey` FOREIGN KEY (`mowerId`) REFERENCES `Mower`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
