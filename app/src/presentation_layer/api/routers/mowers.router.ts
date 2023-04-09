10241a9d446c42ad294c84ee4611c25b0d7cece4
boiledsauce <85931509+boiledsauce@users.noreply.github.com>
2023-04-09 19:47:57 +0200

    Implemented changes to the structure of the REST API with Hasan

--- app/src/presentation_layer/api/routers/mowers.router.ts
+++ app/src/presentation_layer/api/routers/mowers.router.ts
@@ -41,5 +41,53 @@ export class MowersRouter extends AbstractRouter {
         }
       },
     );
+
+     /**
+     * Fetch all mowing sessions for a specific mower by its serial number
+     */
+     this.router.get(
+      '/:serial/sessions',
+      async (req: Request, res: Response, next: NextFunction) => {
+        const { serial } = req.params;
+        res.status(200).json(
+          [{
+            sessionId: "123abc",
+            mowerSerial: "1zA321",
+            startTime: new Date().toISOString(),
+            endTime: new Date().toISOString(),
+          },
+          {
+            sessionId: "124abc",
+            mowerSerial: "1zA321",
+            startTime: new Date().toISOString(),
+            endTime: new Date().toISOString(),
+          },
+          {
+            sessionId: "125abc",
+            mowerSerial: "1zA321",
+            startTime: new Date().toISOString(),
+            endTime: new Date().toISOString(),
+          }]
+        )
+      },
+    );
+    
+    /**
+     * Fetch entire boundaries for all stored mowing sessions 
+     * (full map will be generated the more the mower runs)
+     */
+    this.router.get(
+      '/:serial/maps',
+      async (req: Request, res: Response, next: NextFunction) => {
+        const { serial } = req.params;
+          res.status(200).json(
+            [
+              {x: 10, y: 10}, 
+              {x: 11, y: 11}, 
+              {x: 12, y: 1}
+            ],
+          )
+      },
+    );
   }
 }
--- app/src/presentation_layer/api/routers/sessions.router.ts
+++ app/src/presentation_layer/api/routers/sessions.router.ts
@@ -12,6 +12,9 @@ export class SessionsRouter extends AbstractRouter {
      */
     this.router.post('/start', (req: Request, res: Response) => {
       const { serial } = req.body;
+      res.status(201).json({
+        sessionId: "123abc"
+      });
     });
 
     /**
@@ -19,14 +22,12 @@ export class SessionsRouter extends AbstractRouter {
      */
     this.router.post('/stop', (req: Request, res: Response) => {
       const { serial, sessionId } = req.body;
-    });
-
-    /**
-     * Fetch all mowing sessions for a specific mower by its serial number
-     * URL: /sessions?serial=
-     */
-    this.router.get('/', (req: Request, res: Response) => {
-      const { serial } = req.query;
+      res.status(201).json({
+        sessionId: "123abc",
+        mowerId: "1zA321",
+        startTime: new Date().toISOString(),
+        endTime: new Date().toISOString(),
+      });
     });
 
     /**
@@ -34,6 +35,34 @@ export class SessionsRouter extends AbstractRouter {
      */
     this.router.get('/:id', (req: Request, res: Response) => {
       const { id } = req.params;
+      const now = new Date().toISOString();
+      res.status(200).json({
+        session: {
+          sessionId: "123abc",
+          mowerSerial: "1zA321",
+          startTime: new Date().toISOString(),
+          endTime: new Date().toISOString(),
+        },
+        boundaries: [
+                      {x: 1, y: 1}, 
+                      {x: 1, y: 2}, 
+                      {x: 1, y: 3}, 
+                      {x: 2, y: 1}, 
+                      {x: 3, y: 1}, 
+                      {x: 4, y: 2}
+                    ],
+        positions: [
+                    {x: 10, y: 10}, 
+                    {x: 11, y: 11}, 
+                    {x: 12, y: 1}
+                  ],
+        obstacles: [
+                    {sessionId: "1DcaxzcsxSD", x: 1, y: 1, object: "a plant", imagePath: "http://imgur.com/1", timestamp: now }, 
+                    {sessionId: "1DcaxzcsxSD", x: 1, y: 1, object: "a shoe", imagePath: "http://imgur.com/2", timestamp: now }, 
+                    {sessionId: "1DcaxzcsxSD", x: 1, y: 3, object: "a stone", imagePath: "http://imgur.com/3", timestamp: now }, 
+                    {sessionId: "1DcaxzcsxSD", x: 1, y: 4, object: "a dog", imagePath: "http://imgur.com/4", timestamp: now }
+                  ],
+      })
     });
 
     /**
@@ -42,6 +71,28 @@ export class SessionsRouter extends AbstractRouter {
     this.router.post('/:id/positions', (req: Request, res: Response) => {
       const { id } = req.params; // id is sessionId
       const { sessionId, x, y } = req.body;
+
+      res.status(201).json({
+        id: "jAjdKKk1C3km34kl45",
+        sessionId: "JDjkdKSdkdkd777zDS8x8x",
+        x: 7,
+        y: 8,
+      })
+    });
+
+    /**
+     * Mower sends a request when running over the boundary
+     */
+    this.router.post('/:id/boundaries', (req: Request, res: Response) => {
+      const { id } = req.params; // id is sessionId
+      const { sessionId, x, y } = req.body;
+
+      res.status(201).json({
+        id: "jAjdKKk1C3km34kl45",
+        sessionId: "JDjkdKSdkdkd777zDS8x8x",
+        x: 7,
+        y: 8,
+      })
     });
   }
 }
