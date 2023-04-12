096b9fb0b8fb9479a3943e6c5c007a5467fed64c
boiledsauce <85931509+boiledsauce@users.noreply.github.com>
2023-04-12 15:27:16 +0200

    Refactored a bit

--- app/src/presentation_layer/api/routers/collisions.router.ts
+++ app/src/presentation_layer/api/routers/collisions.router.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
 import AbstractRouter from './abstract-router';
-import { ImageClassificationService } from '../../../data_access_layer/services/image-classification.service';
+import { MowerService } from '../../../business_logic_layer/services';
+import { CollisionService } from '../../../business_logic_layer/services/collision.service';
+
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' })
-const fs = require('fs');
 
 
 
 export class CollisionsRouter extends AbstractRouter {
-  constructor() {
+  constructor(private collisionService: CollisionService) {
     super('/collisions');
   }
 
@@ -35,12 +36,10 @@ export class CollisionsRouter extends AbstractRouter {
         return;
       }
       
-      const filePath: string = req.file.path
-
       try {
-        const base64Image = fs.readFileSync(filePath, 'base64');
-        const imageClassificationResponse = await new ImageClassificationService().detectLabels(base64Image);
-        res.json(imageClassificationResponse);
+        const filePath: string = req.file.path
+        const classifiedImage = await this.collisionService.detectLabels(filePath);
+        res.json(classifiedImage);
       } catch (error: unknown) {
         console.error('Error processing image brother:', (error as any).response.data);
         res.status(500).send('Error processing image');
