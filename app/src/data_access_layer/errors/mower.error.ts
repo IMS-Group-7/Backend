e9c0a02d6148f8cd0209da384b19b345b156dd6a
h-khalaf <h.khalaf723@gmail.com>
2023-04-12 17:57:09 +0200

    updated the mower repository and added comments to each function

--- app/src/data_access_layer/errors/mower.error.ts
+++ app/src/data_access_layer/errors/mower.error.ts
@@ -1,6 +1,6 @@
 import { ResourceAlreadyExistsError } from '../../common/errors';
 
-export class MowerAlreadyExists extends ResourceAlreadyExistsError {
+export class MowerAlreadyExistsError extends ResourceAlreadyExistsError {
   constructor(
     message: string = 'A mower with the specified serial number already exists in the system.',
   ) {
--- app/src/data_access_layer/repositories/mower.repository.ts
+++ app/src/data_access_layer/repositories/mower.repository.ts
@@ -1,10 +1,19 @@
 import { PrismaClient, Mower, Prisma } from '@prisma/client';
-import { DatabaseError, MowerAlreadyExists } from '../errors';
+import { DatabaseError, MowerAlreadyExistsError } from '../errors';
 export { Mower };
 
 export class MowerRepository {
   constructor(private databaseClient: PrismaClient) {}
 
+  /**
+   * Registers a new mower with the specified serial and status.
+   *
+   * @param serial The serial number of the new mower.
+   * @param status The status of the new mower.
+   * @returns The created mower.
+   * @throws MowerAlreadyExistsError if a mower with the same serial number already exists.
+   * @throws DatabaseError if there is any error during the operation.
+   */
   public async create(serial: string, status: string): Promise<Mower> {
     try {
       const now = new Date();
@@ -22,11 +31,18 @@ export class MowerRepository {
         error instanceof Prisma.PrismaClientKnownRequestError &&
         error.code === 'P2002'
       )
-        throw new MowerAlreadyExists();
+        throw new MowerAlreadyExistsError();
       throw new DatabaseError();
     }
   }
 
+  /**
+   * Finds a mower by its serial number.
+   *
+   * @param serial The serial number of the mower to be fetched.
+   * @returns The mower with the specified serial number or null if not found.
+   * @throws DatabaseError if there is any error during the operation.
+   */
   public async findBySerial(serial: string): Promise<Mower | null> {
     try {
       return this.databaseClient.mower.findUnique({
@@ -39,12 +55,20 @@ export class MowerRepository {
     }
   }
 
-  public async updateStatus(serial: string, status: string) {
+  /**
+   * Updates the status of a mower with the specified ID.
+   *
+   * @param id The ID of the mower to update.
+   * @param status The new status of the mower.
+   * @returns The updated mower or null if the mower with the specified ID does not exist.
+   * @throws DatabaseError if there is any error during the operation.
+   */
+  public async updateStatus(id: string, status: string): Promise<Mower | null> {
     try {
       const now = new Date();
       const mower = await this.databaseClient.mower.update({
         where: {
-          serial: serial,
+          id,
         },
         data: {
           status: status,
@@ -53,6 +77,11 @@ export class MowerRepository {
       });
       return mower;
     } catch (error: unknown) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      )
+        return null;
       throw new DatabaseError();
     }
   }
