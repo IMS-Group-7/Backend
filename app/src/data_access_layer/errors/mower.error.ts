ad4f4de1ba8de1a73225b008d9c4628f5e153d52
h-khalaf <h.khalaf723@gmail.com>
2023-04-14 12:02:38 +0200

    Refactored errors in DAL

--- app/src/data_access_layer/errors/mower.error.ts
+++
@@ -1,9 +0,0 @@
-import { ResourceAlreadyExistsError } from '../../common/errors';
-
-export class MowerAlreadyExistsError extends ResourceAlreadyExistsError {
-  constructor(
-    message: string = 'A mower with the specified serial number already exists in the system.',
-  ) {
-    super(message, 'MOWER_ALREADY_EXISTS');
-  }
-}
